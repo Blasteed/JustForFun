ff6b335212326c214a5c6764eab3690dc02521d2
Carfagna Alessandro <81586392+karfee111@users.noreply.github.com>
2022-02-10 21:38:38 +0100

    Added AutoTyper.c - To be finished

---
+++ AutoTyper (& Sender)/AutoTyper.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+char text[10];
+int repeat;
+
+
+int main ()
+{
+
+    printf("Text: ");
+    gets(text)
+    
+    printf("Repeats: ");
+    scanf(repeat)
+
+    
+    
+    return 0
+}
