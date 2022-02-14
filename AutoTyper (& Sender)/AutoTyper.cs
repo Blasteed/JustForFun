e238dc13039bfd3ad67bf4f6639299f9bf21fb79
Alessandro Carfagna <acarfagna03@gmail.com>
2022-02-14 10:58:16 +0100

    Deleted AutoTyper.c, Started coding AutoTyper.cs

---
+++ AutoTyper (& Sender)/AutoTyper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AutoTyper
+{
+    class Program
+    {
+        static void Main (string[] arg)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
