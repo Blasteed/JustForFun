2206f24996eb8271ce19ab21109c362931fe4471
Alessandro Carfagna <acarfagna03@gmail.com>
2022-02-14 10:37:51 +0100

    Emotes were a fail, colored printing, better formatting

--- AutoTyper (& Sender)/AutoTyper.py
+++ AutoTyper (& Sender)/AutoTyper.py
@@ -7,19 +7,18 @@ print('\n\n')
 print(colored("   AUTOTYPER BY KARFEE\n", 'magenta'))
 
 text = input(colored("Text to send: ", 'yellow'))
-emote = input(colored("Emote (click enter if you don't use it): ", 'yellow'))
 repeats = input(colored("Times to repeat: ", 'yellow'))
 
-print('\n\n')
+print('\n')
 
-print(colored(f"SENDING {text} {emote} FOR {repeats} TIMES\nWAITING 5 SECONDS TO START, GET READY !\n\n", 'blue'))
+print(colored(f'SENDING "{text}" FOR {repeats} TIMES\nWAITING 5 SECONDS TO START, GET READY !\n', 'blue'))
 
 time.sleep(5)
 
-print(colored("5 SECONDS PASSED, STARTING !\n\n", 'blue'))
+print(colored("5 SECONDS PASSED, STARTING !\n", 'blue'))
 
 for n in range(int(repeats)):
-    pyautogui.write(f"{text} {emote}")
+    pyautogui.write(f"{text}")
     pyautogui.press('return')
 
 print(colored("PROCESS ENDED, KILLING !", 'blue'))
