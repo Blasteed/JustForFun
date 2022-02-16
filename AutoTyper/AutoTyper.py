d78d46220c76c4286e13c1588509cd04d427331a
Carfagna Alessandro <81586392+karfee111@users.noreply.github.com>
2022-02-16 17:41:04 +0100

    Most of mods on AutoTyper, cleaned files, Added 2 functions at .py (EndMenu, Main), Handles Delays, added comments to understand the code, modified README.md

--- AutoTyper/AutoTyper.py
+++ AutoTyper/AutoTyper.py
@@ -4,23 +4,75 @@ from termcolor import colored
 
 print('\n\n')
 
+# Title's colored printing
 print(colored("   AUTOTYPER BY KARFEE\n", 'magenta'))
 
-text = input(colored("Text to send: ", 'yellow'))
-repeats = input(colored("Times to repeat: ", 'yellow'))
 
-print('\n')
+#Endmenu func
+def EndMenu():
 
-print(colored(f'SENDING "{text}" FOR {repeats} TIMES\nWAITING 5 SECONDS TO START, GET READY !\n', 'blue'))
+    # Menu choice with colored print
+    choice = int(input("Choose an option:\n\n1. Continue\n2. Exit\n\n" + colored("Choice: ", 'yellow')))
 
-time.sleep(5)
+    # First choice, restarting
+    if choice == 1:
+        print('\n')
+        Main()
 
-print(colored("5 SECONDS PASSED, STARTING !\n", 'blue'))
+    # Second choice, exit
+    if choice == 2:
+        print('\n')
+        exit()
 
-for n in range(int(repeats)):
-    pyautogui.write(f"{text}")
-    pyautogui.press('return')
+    # Default choice
+    else:
+        print(colored("\nERROR - CHOOSE A CORRECT OPTION\n", 'red'))
+        EndMenu()
 
-print(colored("PROCESS ENDED, KILLING !", 'blue'))
 
-print('\n')
+def Main():
+    # Colored inputs of vars to run the code
+    text = input(colored("Text to send: ", 'yellow'))
+    repeats = input(colored("Times to repeat: ", 'yellow'))
+    delay = input(colored("Delay's seconds between messages (Leave it empty if you don't use it): ", 'yellow'))
+
+    # Control of var to optimise printings
+    if delay == '':
+        delay = 0
+
+    print('\n')
+
+    # Info printing
+    print(colored(f"SENDING {text} FOR {repeats} TIMES WITH {delay}s OF DELAY", 'blue'))
+
+    # Alert of delay before the starting
+    print(colored('WAITING 5 SECONDS TO START, GET READY !\n', 'blue'))
+
+    # Delay before starting
+    time.sleep(5)
+
+    # Alert starting
+    print(colored("5 SECONDS PASSED, STARTING !\n", 'blue'))
+
+    # It repeats the printing and the sending, respects the delay only if it exists and alert when it is using them
+    for n in range(int(repeats)):
+
+        pyautogui.write(f"{text}")
+
+        if delay > 0:
+            time.sleep(int(delay))
+            print(colored(f"RESPECTING {delay}s OF DELAY - {n + 1}\n", 'blue'))
+        else:
+            pass
+
+        pyautogui.press('return')
+
+    # Alert ending
+    print(colored("PROCESS ENDED, KILLING !\n", 'blue'))
+
+    # Calling EndMenu
+    EndMenu()
+
+
+# First calling Main
+Main()
--- AutoTyper/README.md
+++ AutoTyper/README.md
@@ -1,3 +1,3 @@
 # AutoTyper 
 
-A program that collects a text from the user and the times to repeat it, writes it on a textbox for that times (and gives you 3 seconds from the starting to let you click on the box). Then clicks "ENTER". Destroy your friends!
+A program that collects a text from the user and the times to repeat it, writes it on a textbox for that times (and gives you 5 seconds from the starting to let you click on the text box). Then clicks "ENTER". Destroy your friends! Added the possibility to select a delay between a message sending and another one.
