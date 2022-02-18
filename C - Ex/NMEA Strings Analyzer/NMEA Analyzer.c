bd6785835a24a1adc3b12771aa4492d6ac9132b5
Carfagna Alessandro <81586392+karfee111@users.noreply.github.com>
2022-02-18 19:13:10 +0100

    Initialised Eclipse on 'NMEA Parser', modified .gitignore

--- C - Ex/NMEA Strings Analyzer/NMEA Analyzer.c
+++ C - Ex/NMEA Strings Analyzer/NMEA Analyzer.c
@@ -4,47 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TYPE           1
 #define TIME           2
 #define LATITUDE       3
 #define LATITUDE_DIR   4
 #define LONGITUDE      5
 #define LONGITUDE_DIR  6
-#define CONTROL        7
-#define SATELLITES     8
 #define DILUITION      9
-#define HEIGHT_ASL    10
-#define HASL_UNIT     11
-#define GEOIDE_HEIGHT 12
-#define GH_UNIT       13
-#define STATION_ID    14
-#define CHECKSUM      15
 
-char NMEA_Sentence[100];
-
-char* NMEA_Type;
 char* NMEA_Time;
 char* NMEA_Latitude;
 char* NMEA_Latitude_Dir;
 char* NMEA_Longitude;
 char* NMEA_Longitude_Dir;
+char* NMEA_Satellites_N;
 char* NMEA_Diluition;
 
 int main ()
 {
+    char NMEA_Sentence[100];
+
     int NMEA_Element_Index = 1;
 
     strcpy ( NMEA_Sentence, "$GPGGA,110617.00,41XX.XXXXX,N,00831.54761,W,1,05,2.68,129.0,M,50.1,M,,*42" );
 
     char* slicing = strtok ( NMEA_Sentence, "," );
 
-    NMEA_Type = slicing;
-
-    while (slicing != NULL)
+    while ( slicing != NULL )
     {
         NMEA_Element_Index++;
-
-        // printf ( "%s --- %d\n", slicing, strlen ( slicing ) );
 
         slicing = strtok ( NULL, "," );
 
@@ -54,11 +41,20 @@ int main ()
 
                 NMEA_Time = slicing;
 
+                for (  )
+                {
+                    if (  )
+                }
+
+                printf ( "NMEA_TIME:  %s\n", NMEA_Time );
+
                 break;
 
             case LATITUDE:
 
                 NMEA_Latitude = slicing;
+
+                printf ( "NMEA_LATITUDE:  %s\n", NMEA_Latitude );
 
                 break;
 
@@ -66,11 +62,15 @@ int main ()
 
                 NMEA_Latitude_Dir = slicing;
 
+                printf ( "NMEA_LATITUDE_DIR:  %s\n", NMEA_Latitude_Dir );
+
                 break;
 
             case LONGITUDE:
 
                 NMEA_Longitude = slicing;
+
+                printf ( "NMEA_LONGITUDE:  %s\n", NMEA_Longitude );
 
                 break;
 
@@ -78,11 +78,15 @@ int main ()
 
                 NMEA_Longitude_Dir = slicing;
 
+                printf ( "NMEA_LONGITUDE_DIR:  %s\n", NMEA_Longitude_Dir );
+
                 break;
 
             case DILUITION:
 
                 NMEA_Diluition = slicing;
+
+                printf ( "NMEA_DILUITION:  %s METERS\n", NMEA_Diluition );
 
                 break;
 
@@ -91,8 +95,6 @@ int main ()
                 break;
         }
     }
-
-    printf( "\n\nNMEA_TYPE:  %s\nNMEA_TIME:  %s\nNMEA_LATITUDE:  %s\nNMEA_LATITUDE_DIR:  %s\nNMEA_LONGITUDE:  %s\nNMEA_LONGITUDE_DIR:  %s\nNMEA_DILUITION:  %s\n", NMEA_Type, NMEA_Time, NMEA_Latitude, NMEA_Latitude_Dir, NMEA_Longitude, NMEA_Longitude_Dir, NMEA_Diluition );
 
     return 0;
 }
