ac7a21f04a659bcfdd47a45118350a4698be34bf
Carfagna Alessandro <81586392+karfee111@users.noreply.github.com>
2022-02-19 12:02:54 +0100

    Perfectioned the code and deleted useless functions, perfectioned castings and var names, fixed some allocations and castings

--- C - Ex/NMEA Parser/NMEA Analyzer.c
+++ C - Ex/NMEA Parser/NMEA Analyzer.c
@@ -1,100 +1,83 @@
 // YourKarfee - NMEA Sentences Slicer for School
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "NMEA.h"
 
 
-int sNMEASatellitesNumb;
-int sNMEATime;
+int iNMEASatellitesNumb;
 
-double sNMEADiluition;
-double sNMEALatitude;
-double sNMEALongitude;
+double dNMEADiluition;
+double dNMEALatitude;
+double dNMEALongitude;
 
-char sNMEALatitudeCard;
-char sNMEALongitudeCard;
+char cNMEALatitudeCard;
+char cNMEALongitudeCard;
 
 char sNMEAType[6];
+char sNMEATime[6];
 
 
-void NMEAParser ()
+void NMEA_Parser ()
 {
-    char NMEA_Sentence[100];
+    char sNMEASentence[100];
 
-    int NMEA_Element_Index = 1;
+    int imNMEAElementIndex = 1;
 
-    strcpy ( NMEA_Sentence, "$GPGGA,134658.00,5106.9792,N,11402.3003,W,2,09,1.0,1048.47,M,-16.27,M,08,AAAA*60" );
+    strcpy ( sNMEASentence, "$GPGGA,001038.00,3334.2313457,N,11211.0576940,W,2,04,5.4,354.682,M,- 26.574,M,7.0,0138*79" );
 
-    char* sSentenceSlicer = strtok ( NMEA_Sentence, "," );
+    char* psSentenceSlicer = strtok ( sNMEASentence, "," );
 
-    sscanf ( sSentenceSlicer, "%s", sNMEAType );
+    sscanf ( psSentenceSlicer, "%s", sNMEAType );
 
-    printf ( "%d:  %s\n", NMEA_Element_Index, sNMEAType );
-
-    while ( sSentenceSlicer != NULL )
+    while ( psSentenceSlicer != NULL )
     {
-        NMEA_Element_Index++;
+        imNMEAElementIndex++;
 
-        sSentenceSlicer = strtok ( NULL, "," );
+        psSentenceSlicer = strtok ( NULL, "," );
 
-        switch ( NMEA_Element_Index )
+        switch ( imNMEAElementIndex )
         {
             case TIME:
 
-                sscanf ( sSentenceSlicer, "%d", &sNMEATime );
-
-                printf ( "%d:  %d\n", NMEA_Element_Index, sNMEATime );
+                sscanf ( psSentenceSlicer, "%s", sNMEATime );
 
                 break;
 
             case LATITUDE:
 
-                sscanf ( sSentenceSlicer, "%lg", &sNMEALatitude );
-
-                printf ( "%d:  %lg\n", NMEA_Element_Index, sNMEALatitude );
+                sscanf ( psSentenceSlicer, "%lg", &dNMEALatitude );
 
                 break;
 
             case LATITUDE_DIR:
 
-                sscanf ( sSentenceSlicer, "%c", &sNMEALatitudeCard );
-
-                printf ( "%d:  %c\n", NMEA_Element_Index, sNMEALatitudeCard );
+                sscanf ( psSentenceSlicer, "%c", &cNMEALatitudeCard );
 
                 break;
 
             case LONGITUDE:
 
-                sscanf ( sSentenceSlicer, "%lg", &sNMEALongitude );
-
-                printf ( "%d:  %lg\n", NMEA_Element_Index, sNMEALongitude );
+                sscanf ( psSentenceSlicer, "%lg", &dNMEALongitude );
 
                 break;
 
             case LONGITUDE_DIR:
 
-                sscanf ( sSentenceSlicer, "%c", &sNMEALongitudeCard );
-
-                printf ( "%d:  %c\n", NMEA_Element_Index, sNMEALongitudeCard );
+                sscanf ( psSentenceSlicer, "%c", &cNMEALongitudeCard );
 
                 break;
 
             case SATELLITES_NUMB:
 
-                sscanf ( sSentenceSlicer, "%d", &sNMEASatellitesNumb );
-
-                printf ( "%d:  %d\n", NMEA_Element_Index, sNMEASatellitesNumb );
+                sscanf ( psSentenceSlicer, "%d", &iNMEASatellitesNumb );
 
                 break;
 
             case DILUITION:
 
-                sscanf ( sSentenceSlicer, "%lg", &sNMEADiluition );
-
-                printf ( "%d:  %lg\n", NMEA_Element_Index, sNMEADiluition );
+                sscanf ( psSentenceSlicer, "%lg", &dNMEADiluition );
 
                 break;
 
